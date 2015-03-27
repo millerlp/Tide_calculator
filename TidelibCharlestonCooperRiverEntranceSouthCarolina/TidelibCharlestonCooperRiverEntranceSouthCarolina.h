2ec61dbd00d6cc028695597a2362af7964d640aa
Luke Miller <millerlp@gmail.com>
2015-03-27 11:36:05 -0700

    Updated Charleston Cooper River tide library to work with newest version of Arduino IDE v1.6.1

--- TidelibCharlestonCooperRiverEntranceSouthCarolina/TidelibCharlestonCooperRiverEntranceSouthCarolina.h
+++ TidelibCharlestonCooperRiverEntranceSouthCarolina/TidelibCharlestonCooperRiverEntranceSouthCarolina.h
@@ -1,7 +1,7 @@
 /*  TidelibCharlestonCooperRiverEntranceSouthCarolina.h 
   A library for calculating the current tide height at 
    Charleston, Cooper River Entrance, South Carolina 
-  Luke Miller, June 2012
+  Luke Miller, 2015-03-27 
   https://github.com/millerlp/Tide_calculator
 */ 
  
